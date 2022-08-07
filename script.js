7e04b1aaf6ee28b6902cda94fcf144a214ff6f6a
Diego <diegoescalona1818@gmail.com>
2022-08-07 15:15:32 -0300

    third commit

--- script.js
+++ script.js
@@ -33,29 +33,19 @@
 const d = document,
   $grande = d.querySelector(".grande"),
   $punto = d.querySelectorAll(".punto"),
-  $flecha = d.querySelectorAll(".flecha");
+  $flechaLeft = d.querySelector("#flecha-left"),
+  $flechaRight = d.querySelector("#flecha-right");
+
+let ubication = 1;
 
 $punto.forEach((cadaPunto, i) => {
   $punto[i].addEventListener("click", () => {
     let position = i,
-      operacion = position * -50;
+      calculation = position * -50;
 
-    $grande.style.transform = `translateX(${operacion}%)`;
+    ubication = i + 1;
 
-    $punto.forEach((cadaPunto, i) => {
-      $punto[i].classList.remove("activo");
-    });
-    $punto[i].classList.add("activo");
-  });
-});
-
-$flecha.forEach((cadaFlecha, i) => {
-  $flecha[i].addEventListener("click", (e) => {
-    e.preventDefault();
-    let position = i,
-      operacion = position * -50;
-
-    $grande.style.transform = `translateX(${operacion}%)`;
+    $grande.style.transform = `translateX(${calculation}%)`;
 
     $punto.forEach((cadaPunto, i) => {
       $punto[i].classList.remove("activo");
@@ -63,3 +53,83 @@ $flecha.forEach((cadaFlecha, i) => {
     $punto[i].classList.add("activo");
   });
 });
+
+const next = () => {
+  if (ubication < $punto.length) {
+    calculation = ubication * -50;
+    $grande.style.transform = `translateX(${calculation}%)`;
+
+    $punto.forEach((cadaPunto, i) => {
+      $punto[i].classList.remove("activo");
+    });
+    $punto[ubication].classList.add("activo");
+    ++ubication;
+  }
+};
+
+const prev = () => {
+  if (ubication <= $punto.length && ubication > 1) {
+    calculation = ubication * -50 + 100;
+    $grande.style.transform = `translateX(${calculation}%)`;
+    --ubication;
+
+    $punto.forEach((cadaPunto, i) => {
+      $punto[i].classList.remove("activo");
+    });
+    $punto[ubication - 1].classList.add("activo");
+  }
+};
+
+$flechaRight.addEventListener("click", (e) => {
+  e.preventDefault();
+  next();
+});
+
+$flechaLeft.addEventListener("click", (e) => {
+  e.preventDefault();
+  prev();
+});
+
+let posX1, posX2;
+
+$grande.addEventListener("mousedown", dragStart);
+
+$grande.addEventListener("touchstart", dragStart);
+$grande.addEventListener("touchmove", dragMove);
+$grande.addEventListener("touchend", dragEnd);
+
+function dragStart(e) {
+  e.preventDefault();
+
+  if (e.type === "touchstart") {
+    posX1 = e.touches[0].clientX;
+  } else {
+    posX1 = e.clientX;
+
+    d.onmouseup = dragEnd;
+    d.onmousemove = dragMove;
+  }
+}
+
+function dragMove(e) {
+  if (e.type === "touchmove") {
+    posX2 = posX1 - e.touches[0].clientX;
+    posX1 = e.touches[0].clientX;
+  } else {
+    posX2 = posX1 - e.clientX;
+    posX1 = e.clientX;
+  }
+}
+
+function dragEnd() {
+  if (posX2 >= 1) {
+    next();
+    posX2 = 0;
+  } else if (posX2 <= -1) {
+    prev();
+    posX2 = 0;
+  }
+
+  d.onmouseup = null;
+  d.onmousemove = null;
+}
